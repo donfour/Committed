821a1de64eecf47e7c604aa822bc7e1bfc52e95c
donfour <donovanso@yahoo.com.hk>
2017-09-11 22:54:47 +0800

    added edit button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,8 @@ import { FONT_COLOR, COMPLETED_FONT_COLOR } from '../constants/constants';
 
 export default class TodoItem extends React.Component{
   state = {
-    isOpened: false
+    isOpened: false,
+    displayEditButton: false
   }
 
   toggleOpen(){
@@ -33,8 +34,22 @@ export default class TodoItem extends React.Component{
             className="todo-content"
             onClick={()=>{this.toggleOpen()}}
             style={this.props.completed ? {color: COMPLETED_FONT_COLOR} : {color: FONT_COLOR}}
+            onMouseOver={()=>{this.setState({displayEditButton:true})}}
+            onMouseOut={()=>{this.setState({displayEditButton:false})}}
           >
             {this.props.name}
+
+            <div
+              className="edit-button"
+              style={ this.state.displayEditButton ? null : { display: 'none' } }
+            >
+              <svg width="100%" height="100%" viewBox="0 0 528.899 528.899">
+                <g>
+                	<path d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981 c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611 C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069 L27.473,390.597L0.3,512.69z"/>
+                </g>
+              </svg>
+            </div>
+
           </div>
         </div>
         <Collapse isOpened={this.state.isOpened}>
