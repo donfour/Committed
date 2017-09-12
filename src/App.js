fd9b256714ed7944d20059107aeb0bc990acf788
donfour <donovanso@yahoo.com.hk>
2017-09-12 23:42:38 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import TodoList from './components/TodoList';
+import GithubIcon from './components/GithubIcon';
 
 class App extends Component {
   constructor(props){
@@ -200,12 +201,14 @@ class App extends Component {
         </button>
 
         <div className="fox-icon-container">
-          <a href="https://github.com/donfour/Committed" target="_blank">
+          <a href="https://donfour.github.io/donovanso/" target="_blank">
             <svg width="100%" height="100%" viewBox="0 0 200 200">
               <path className="fox" d="M100,192.8c-24.2-47.9-75.3-71.2-75.3-71.2V15.2c0,0,46.8,4.5,46.8,42.2h57c0-37.6,46.8-42.2,46.8-42.2v106.5 C175.3,121.6,124.2,145,100,192.8z"/>
             </svg>
           </a>
         </div>
+
+        <GithubIcon/>
 
         <TodoList
           editing={this.state.editing}
