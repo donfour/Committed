30bcd28766b6931e3d5891faf0a561613205676b
donfour <donovanso@yahoo.com.hk>
2017-10-20 19:46:31 +0800

    improved themes 0 to 5

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     // initialize state
     const todos = JSON.parse(localStorage.getItem('todos')) || [];
     const counter = localStorage.getItem('counter') || 0;
-    const themeNumber = localStorage.getItem('themeNumber') || "theme-4";
+    const themeNumber = localStorage.getItem('themeNumber') || "theme-5";
 
     var timeNow = new Date();
     console.log('timeNow.toDateString()', timeNow.toDateString());
